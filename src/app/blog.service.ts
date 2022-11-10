b11e2a5def93ea8b3967f31975173613611a1b40
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-10 11:39:34 +0530

    Added get blogs method in blog service

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,8 @@ export class BlogService {
 
   URL: string = "http://localhost:3000/blogs"
 
-  
+  // To get all the Blogs from JSON File
+  getAllBlogs(): Observable<Array<Object>> {
+    return this.httpclient.get<Array<Object>>(this.URL);
+  }
 }
