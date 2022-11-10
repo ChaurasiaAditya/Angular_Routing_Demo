d0e6b254e08723ae8ed9554d74c4e695b85d9b12
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-10 17:00:49 +0530

    Added gaurd in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './add/add.component';
+import { AdminGuard } from './admin.guard';
 import { AdminComponent } from './admin/admin.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { DeleteComponent } from './delete/delete.component';
 import { EditComponent } from './edit/edit.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { NotesComponent } from './notes/notes.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { ViewoneblogComponent } from './viewoneblog/viewoneblog.component';
@@ -29,12 +31,17 @@ const routes: Routes = [
     component: ViewoneblogComponent
   },
   {
+    path: "login",
+    component: LoginComponent
+  },
+  {
     path: "viewonenote/:id",
     component: ViewonenoteComponent
   },
   {
     path: "admin",
     component: AdminComponent,
+    canActivate:[AdminGuard],
     children:
       [
         {
