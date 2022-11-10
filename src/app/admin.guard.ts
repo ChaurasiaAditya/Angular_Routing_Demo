21d252d73c0d90d0dede2e1ce718be7ded161266
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-10 17:40:26 +0530

    added gaurd condition in admin gaurd

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+
+  constructor(private loginservice:LoginService, private router:Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if(this.loginservice.allowlogin){
+        return true;
+      } else {
+        return false;
+      }
   }
   
 }
